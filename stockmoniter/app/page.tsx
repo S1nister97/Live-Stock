3bb5e3f0e6b51ae9985e7ac5ce6a8f1f7af011df
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-10-02 11:06:42 +0530

    Added required files

---
+++ stockmoniter/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "../components/ui/button";
+import React from 'react';
+const Page = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <Button> Click here </Button>
+        </div>
+    )
+}
+export default Page
